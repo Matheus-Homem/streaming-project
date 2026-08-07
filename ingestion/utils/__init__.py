8e24dae4b997a08e382e2153c074170804e0530d
matheus.homem <matheuschomem@hotmail.com>
2026-08-07 19:12:28 -0300

    feat: add BoundedUniqueTracker to __all__ in utils module

--- ingestion/utils/__init__.py
+++ ingestion/utils/__init__.py
@@ -1,4 +1,5 @@
 from ingestion.utils.exceptions import RateLimitError
 from ingestion.utils.timer import RetryTimer
+from ingestion.utils.tracker import BoundedUniqueTracker
 
-__all__ = ["RetryTimer", "RateLimitError"]
+__all__ = ["RetryTimer", "RateLimitError", "BoundedUniqueTracker"]
