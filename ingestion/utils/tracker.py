7f046ca6a91000486ed146b7f70db32d9981fe53
matheus.homem <matheuschomem@hotmail.com>
2026-08-10 11:51:41 -0300

    feat: fix max_size reference in BoundedUniqueTracker and add unit tests

--- ingestion/utils/tracker.py
+++ ingestion/utils/tracker.py
@@ -1,11 +1,11 @@
 from collections import deque
 
+
 class BoundedUniqueTracker:
 
     def __init__(self, max_size: int):
         self.deque = deque(maxlen=max_size)
         self.memory = set()
-        self.max_size = max_size
 
     def is_duplicated(self, value: str) -> bool:
         if value in self.memory:
@@ -13,7 +13,7 @@ class BoundedUniqueTracker:
         return False
 
     def record(self, value: str) -> bool:
-        if len(self.deque) >= self.max_size:
+        if len(self.deque) >= self.deque.maxlen:
             oldest = self.deque.popleft()
             self.memory.remove(oldest)
 
