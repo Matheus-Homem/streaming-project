8cdcded9530be2b86b9e89b4c523d524051f5209
matheus.homem <matheuschomem@hotmail.com>
2026-08-19 06:21:07 -0300

    feat: add __init__.py files for normalization adapters and test fixtures

---
+++ flink/normalization/adapters/__init__.py
@@ -0,0 +1,4 @@
+from flink.normalization.adapters.engine import NormalizationEngine
+from flink.normalization.adapters.evaluator import NormalizationJmespathEvaluator
+
+__all__ = [NormalizationEngine, NormalizationJmespathEvaluator]
