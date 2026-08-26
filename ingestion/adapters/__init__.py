5a5fb6f8a5bdd8153017b7093549d269dc42c0d6
matheus.homem <matheuschomem@hotmail.com>
2026-08-26 15:46:47 -0300

    feat: add YamlSourceConfigRepository to the public API of the ingestion adapters

--- ingestion/adapters/__init__.py
+++ ingestion/adapters/__init__.py
@@ -1,6 +1,7 @@
 from ingestion.adapters.client import IngestionClient
 from ingestion.adapters.engine import IngestionEngine
 from ingestion.adapters.producer import IngestionProducer
+from ingestion.adapters.source_config_repository import YamlSourceConfigRepository
 from ingestion.adapters.tracker import IngestionTracker
 
 __all__ = [
@@ -8,4 +9,5 @@ __all__ = [
     "IngestionEngine",
     "IngestionProducer",
     "IngestionTracker",
+    "YamlSourceConfigRepository",
 ]
